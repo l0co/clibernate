05081617d9de0d0b8e9212bf71c4ca2558298363
lukasz <lukasz.frankowski@gmail.com>
2014-12-24 10:52:13 +0100

    disable log4j debug + add cli

--- src/main/java/com/blogspot/lifeinide/clibernate/Clibernate.java
+++ src/main/java/com/blogspot/lifeinide/clibernate/Clibernate.java
@@ -1,8 +1,6 @@
 package com.blogspot.lifeinide.clibernate;
 
 import com.blogspot.lifeinide.clibernate.services.ICliService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -13,11 +11,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class Clibernate {
 
-	public static Logger logger = LoggerFactory.getLogger(Clibernate.class);
-
 	public static void main(String [] args) {
-		logger.debug("Starting Spring application context");
-
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 
 		// TODOLF remove
